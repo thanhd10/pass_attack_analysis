b0aa9dcc875588923ce0d2381c053877643e8ad0
thanhd10 <thanh_dinh@outlook.de>
2021-12-06 15:36:08 +0200

    Update work title and add new visualization

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import pandas as pd
 
 from keystroke_analysis import analyze_pattern_for_keystrokes
 from utils.read_data import read_password_measurements
+from utils.visualize import visualize_sensor_pattern
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 if __name__ == '__main__':
-    pattern = read_password_measurements("data/input_4444.json")
+    pattern = read_password_measurements("data/simple_samples/input_3333.json")
     keystrokes_input = analyze_pattern_for_keystrokes(pattern)
+
+    # visualize_sensor_data(pattern)
+    visualize_sensor_pattern(pattern, "pattern_3333.svg")
+
+    for keystroke in keystrokes_input:
+        print(keystroke.keystroke_confidences)
--- utils/visualize.py
+++ utils/visualize.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+from definitions import TIMESTAMP_COL
+
 
 def visualize_sensor_data(readings: pd.DataFrame, start=0, end=float('inf')):
     """
@@ -9,10 +11,35 @@ def visualize_sensor_data(readings: pd.DataFrame, start=0, end=float('inf')):
     """
     sns.set_style(style='ticks')
     fig, axes = plt.subplots(3, 1, figsize=(12, 6))
-    target_df = readings[(readings['time_difference'] >= start) & (readings['time_difference'] <= end)]
-    sns.lineplot(y='acc_z', x='time_difference', data=target_df, ax=axes[0], palette='tab10', linewidth=2, dashes=True)
-    sns.lineplot(y='gyr_y', x='time_difference', data=target_df, ax=axes[1], palette='tab10', linewidth=2, dashes=True)
-    sns.lineplot(y='gyr_x', x='time_difference', data=target_df, ax=axes[2], palette='tab10', linewidth=2, dashes=True)
+    target_df = readings[(readings[TIMESTAMP_COL] >= start) & (readings[TIMESTAMP_COL] <= end)]
+    sns.lineplot(y='acc_z', x=TIMESTAMP_COL, data=target_df, ax=axes[0], palette='tab10', linewidth=2, dashes=True)
+    sns.lineplot(y='gyr_y', x=TIMESTAMP_COL, data=target_df, ax=axes[1], palette='tab10', linewidth=2, dashes=True)
+    sns.lineplot(y='gyr_x', x=TIMESTAMP_COL, data=target_df, ax=axes[2], palette='tab10', linewidth=2, dashes=True)
     plt.tight_layout()
 
+    plt.show()
+
+
+def visualize_sensor_pattern(pattern: pd.DataFrame,
+                             target_path: str,
+                             columns_to_plot=None,
+                             x_label='Time in seconds',
+                             y_label='Distribution'):
+    # plot pattern of roundabout
+    if columns_to_plot is None:
+        columns_to_plot = ["acc_z", "gyr_x", "gyr_y"]
+
+    fig, ax = plt.subplots(figsize=(12, 3))
+    sns.set_style(style='ticks')
+    sns.lineplot(x=TIMESTAMP_COL, y='value', hue='variable',
+                 data=pd.melt(pattern[columns_to_plot + [TIMESTAMP_COL]], [TIMESTAMP_COL]),
+                 palette='tab10', linewidth=2, dashes=True).set(xlabel=x_label,
+                                                                ylabel=y_label)
+
+    # design optimizations
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.tight_layout()
+    fig.savefig(target_path, format='svg', dpi=1200)
     plt.show()
